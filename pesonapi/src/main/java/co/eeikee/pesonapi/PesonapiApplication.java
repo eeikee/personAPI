56c02b3c53bbe6f4e608b57a47c8c6802acff6a2
Eike <konumaeike5618@hotmail.com>
2020-09-18 23:50:09 -0300

    created personaAPI project

---
+++ pesonapi/src/main/java/co/eeikee/pesonapi/PesonapiApplication.java
@@ -0,0 +1,13 @@
+package co.eeikee.pesonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesonapiApplication.class, args);
+	}
+
+}
---
+++ pesonapi/src/test/java/co/eeikee/pesonapi/PesonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.eeikee.pesonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
